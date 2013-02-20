0563c5bd2201c1f0fe1fb7dc3aca0748a22c7d7e
Eduard Burtescu <edy.burt@gmail.com>
2013-02-20 10:27:58 +0200

    wUI: replaced the inline jQuery watch plugin with a newer one.

--- static/js/wUI.js
+++ static/js/wUI.js
@@ -1,108 +1,3 @@
-(function($){
-$.fn.watch = function (props, func, interval, id) {
-    /// <summary>
-    /// Allows you to monitor changes in a specific
-    /// CSS property of an element by polling the value.
-    /// when the value changes a function is called.
-    /// The function called is called in the context
-    /// of the selected element (ie. this)
-    /// </summary>
-    /// <param name="prop" type="String">CSS Properties to watch sep. by commas</param>
-    /// <param name="func" type="Function">
-    /// Function called when the value has changed.
-    /// </param>
-    /// <param name="interval" type="Number">
-    /// Optional interval for browsers that don't support DOMAttrModified or propertychange events.
-    /// Determines the interval used for setInterval calls.
-    /// </param>
-    /// <param name="id" type="String">A unique ID that identifies this watch instance on this element</param>
-    /// <returns type="jQuery" />
-    if (!interval)
-        interval = 100;
-    if (!id)
-        id = "_watcher";
-
-    return this.each(function () {
-        var _t = this;
-        var el$ = $(this);
-        var fnc = function () { __watcher.call(_t, id) };
-
-        var data = { id: id,
-            props: props.split(","),
-            vals: [props.split(",").length],
-            func: func,
-            fnc: fnc,
-            origProps: props,
-            interval: interval,
-            intervalId: null
-        };
-        // store initial props and values
-        $.each(data.props, function (i) { data.vals[i] = el$.css(data.props[i]); });
-
-        el$.data(id, data);
-
-        hookChange(el$, id, data);
-    });
-
-    function hookChange(el$, id, data) {
-        el$.each(function () {
-            var el = $(this);
-            if (typeof (el.get(0).onpropertychange) == "object")
-                el.bind("propertychange." + id, data.fnc);
-            else if ($.browser.mozilla)
-                el.bind("DOMAttrModified." + id, data.fnc);
-            else
-                data.intervalId = setInterval(data.fnc, interval);
-        });
-    }
-    function __watcher(id) {
-        var el$ = $(this);
-        var w = el$.data(id);
-        if (!w) return;
-        var _t = this;
-
-        if (!w.func)
-            return;
-
-        // must unbind or else unwanted recursion may occur
-        el$.unwatch(id);
-
-        var changed = false;
-        var i = 0;
-        for (i; i < w.props.length; i++) {
-            var newVal = el$.css(w.props[i]);
-            if (w.vals[i] != newVal) {
-                w.vals[i] = newVal;
-                changed = true;
-                break;
-            }
-        }
-        if (changed)
-            w.func.call(_t, w, i);
-
-        // rebind event
-        hookChange(el$, id, w);
-    }
-}
-$.fn.unwatch = function (id) {
-    this.each(function () {
-        var el = $(this);
-        var data = el.data(id);
-        try {
-            if (typeof (this.onpropertychange) == "object")
-                el.unbind("propertychange." + id, data.fnc);
-            else if ($.browser.mozilla)
-                el.unbind("DOMAttrModified." + id, data.fnc);
-            else
-                clearInterval(data.intervalId);
-        }
-        // ignore if element was already unbound
-        catch (e) { }
-    });
-    return this;
-}
-})(jQuery);
-
 window.$ui = {};
 
 $ui.classes = {
@@ -152,18 +47,21 @@ $ui.VText = function VText(element) {
         element = $('<div>').text(element);
     else if(element.length > 1)
         element = $('<div>').append(element);
+
     var unwatchTimeout = 0;
-    return $('<div>').append(element.addClass('wui-vertical-text').watch('width', function() {
+    element.addClass('wui-vertical-text').on('css-change', function watcher() {
         // HACK TODO find a better way to detect the update of the vertical text
-        var div = $(this), w = div.width(), h = div.height();
+        var w = element.width(), h = element.height();
         if(w && h) {
-            div.css({top: w}).parent().css({width: h, height: w, overflow: 'hidden'});
+            element.css({top: w}).parent().css({width: h, height: w, overflow: 'hidden'});
             if(unwatchTimeout)
                 clearTimeout(unwatchTimeout);
             // Give it 10 seconds to load.
-            unwatchTimeout = setTimeout(element.unwatch.bind(element, '_watcher'), 10000);
+            unwatchTimeout = setTimeout(element.off.bind(element, 'css-change', watcher), 10000);
         }
-    }));
+    }).csswatch({props: 'width', props_functions: {width: 'width()'}});
+
+    return $('<div>').append(element);
 };
 
 $ui.MenuBar = function MenuBar(menus) {
